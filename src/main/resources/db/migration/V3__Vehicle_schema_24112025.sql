2fd45af2835668feefb6320fd579440812546695
Gunnar Gudlaugsson <gunnar.gudlaugsson@egde.no>
2025-11-24 14:55:29 +0100

    Add geo types take 2

--- src/main/resources/db/migration/V3__Vehicle_schema_24112025.sql
+++ src/main/resources/db/migration/V3__Vehicle_schema_24112025.sql
@@ -1,36 +1,3 @@
-ALTER SCHEMA public OWNER TO sobek;
-
---
--- TOC entry 4734 (class 0 OID 0)
--- Dependencies: 8
--- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: sobek
---
-
-COMMENT ON SCHEMA public IS 'standard public schema';
-
-        CREATE EXTENSION IF NOT EXISTS postgis WITH SCHEMA public;
-
-
---
--- Name: EXTENSION postgis; Type: COMMENT; Schema: -; Owner:
---
-
-COMMENT ON EXTENSION postgis IS 'PostGIS geometry, geography, and raster spatial types and functions';
-
-
---
--- Name: postgis_topology; Type: EXTENSION; Schema: -; Owner:
---
-
-CREATE EXTENSION IF NOT EXISTS postgis_topology WITH SCHEMA topology;
-
-
---
--- Name: EXTENSION postgis_topology; Type: COMMENT; Schema: -; Owner:
---
-
-COMMENT ON EXTENSION postgis_topology IS 'PostGIS topology spatial types and functions';
-
 
 SET default_tablespace = '';
 

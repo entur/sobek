33ac3ac156d9120134069a3402dbad5cc5b4e18c
Gunnar Gudlaugsson <gunnar.gudlaugsson@egde.no>
2025-11-24 15:28:59 +0100

    Remove schema version table

--- src/main/resources/db/migration/V3__Vehicle_schema_24112025.sql
+++ src/main/resources/db/migration/V3__Vehicle_schema_24112025.sql
@@ -756,18 +756,18 @@ ALTER SEQUENCE public.persistable_polygon_seq OWNER TO sobek;
 -- Name: schema_version; Type: TABLE; Schema: public; Owner: sobek
 --
 
-CREATE TABLE public.schema_version (
-                                       installed_rank integer NOT NULL,
-                                       version character varying(50),
-                                       description character varying(200) NOT NULL,
-                                       type character varying(20) NOT NULL,
-                                       script character varying(1000) NOT NULL,
-                                       checksum integer,
-                                       installed_by character varying(100) NOT NULL,
-                                       installed_on timestamp without time zone DEFAULT now() NOT NULL,
-                                       execution_time integer NOT NULL,
-                                       success boolean NOT NULL
-);
+-- CREATE TABLE public.schema_version (
+--                                        installed_rank integer NOT NULL,
+--                                        version character varying(50),
+--                                        description character varying(200) NOT NULL,
+--                                        type character varying(20) NOT NULL,
+--                                        script character varying(1000) NOT NULL,
+--                                        checksum integer,
+--                                        installed_by character varying(100) NOT NULL,
+--                                        installed_on timestamp without time zone DEFAULT now() NOT NULL,
+--                                        execution_time integer NOT NULL,
+--                                        success boolean NOT NULL
+-- );
 
 
 ALTER TABLE public.schema_version OWNER TO sobek;

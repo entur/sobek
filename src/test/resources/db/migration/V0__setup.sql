bbad5d1efa29fcb56857fd490be2afa6e75b325d
Kjetil Midtlie <kjetil.midtlie@gmail.com>
2026-01-07 15:55:37 +0100

    graceful role creation

--- src/test/resources/db/migration/V0__setup.sql
+++ src/test/resources/db/migration/V0__setup.sql
@@ -1,2 +1,9 @@
-CREATE USER sobek with PASSWORD 'sobek';
-CREATE USER postgres;
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'sobek') THEN
+        CREATE USER sobek WITH PASSWORD 'sobek';
+    END IF;
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'postgres') THEN
+        CREATE ROLE postgres;
+    END IF;
+END $$;

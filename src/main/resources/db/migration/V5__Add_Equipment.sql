5c9a28afb0eded9e97947b8def7cb7021bdd1cb7
Gunnar Gudlaugsson <gunnar.gudlaugsson@egde.no>
2026-01-08 11:43:28 +0100

    Table structure, before making DeckSpace a combined entity for PassengerSpace, DeckSpace  and OtherDeckSpace

--- src/main/resources/db/migration/V5__Add_Equipment.sql
+++ src/main/resources/db/migration/V5__Add_Equipment.sql
@@ -1,3 +1,23 @@
+ALTER TABLE passenger_spot
+    DROP CONSTRAINT fk2pjm2pakaifla5yq7r1cxqhhc;
+
+ALTER TABLE passenger_space_passenger_spots
+    DROP CONSTRAINT fk7heqymvtxduigg4yh6p0i0bti;
+
+ALTER TABLE passenger_spot_key_values
+    DROP CONSTRAINT fkli3dtmouxlvsui6gosuq80ug3;
+
+ALTER TABLE passenger_spot_key_values
+    DROP CONSTRAINT fksb8dp7sv2tk61e5y8jsn65nd7;
+
+CREATE SEQUENCE IF NOT EXISTS sequence_per_table_generator START WITH 1 INCREMENT BY 50;
+
+CREATE TABLE deck_spot_columns
+(
+    deck_id         BIGINT NOT NULL,
+    spot_columns_id BIGINT NOT NULL
+);
+
 CREATE TABLE equipment
 (
     id                          BIGINT      NOT NULL,
@@ -105,6 +125,16 @@ CREATE TABLE equipment
     CONSTRAINT pk_equipment PRIMARY KEY (id)
 );
 
+CREATE SEQUENCE equipment_seq
+    START WITH 1
+    INCREMENT BY 10
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE equipment_seq OWNER TO sobek;
+
 
 CREATE TABLE equipment_key_values
 (
@@ -114,31 +144,10 @@ CREATE TABLE equipment_key_values
     CONSTRAINT pk_equipment_keyvalues PRIMARY KEY (equipment_id, key_values_key)
 );
 
-ALTER TABLE equipment_key_values
-    ADD CONSTRAINT uc_equipment_key_values_keyvalues UNIQUE (key_values_id);
-
-ALTER TABLE equipment_key_values
-    ADD CONSTRAINT fk_equkeyval_on_equipment FOREIGN KEY (equipment_id) REFERENCES equipment (id);
-
-ALTER TABLE equipment_key_values
-    ADD CONSTRAINT fk_equkeyval_on_value FOREIGN KEY (key_values_id) REFERENCES value (id);
-
-
-CREATE SEQUENCE IF NOT EXISTS public.equipment_seq
-    INCREMENT 10
-    START 1
-    MINVALUE 1
-    MAXVALUE 9223372036854775807
-    CACHE 1;
-
-ALTER SEQUENCE public.equipment_seq
-    OWNER TO sobek;
-
-CREATE TABLE luggage_spot
+CREATE TABLE locatable_spot
 (
-    id                      BIGINT NOT NULL,
-    is_accessible_on_voyage BOOLEAN,
-    height_from_floor       DECIMAL,
+    id                      BIGINT      NOT NULL,
+    dtype                   VARCHAR(31) NOT NULL,
     locatable_spot_type     VARCHAR(255),
     spot_column_id          BIGINT,
     spot_row_id             BIGINT,
@@ -152,7 +161,7 @@ CREATE TABLE luggage_spot
     changed_by              VARCHAR(255),
     created                 TIMESTAMP WITHOUT TIME ZONE,
     changed                 TIMESTAMP WITHOUT TIME ZONE,
-    version                 BIGINT NOT NULL,
+    version                 BIGINT      NOT NULL,
     netex_id                VARCHAR(255),
     label_value             VARCHAR(255),
     label_lang              VARCHAR(5),
@@ -164,33 +173,23 @@ CREATE TABLE luggage_spot
     private_code_type       VARCHAR(255),
     from_date               TIMESTAMP WITHOUT TIME ZONE,
     to_date                 TIMESTAMP WITHOUT TIME ZONE,
-    CONSTRAINT pk_luggagespot PRIMARY KEY (id)
+    is_accessible_on_voyage BOOLEAN,
+    height_from_floor       DECIMAL,
+    table_type              VARCHAR(255),
+    has_armrest             BOOLEAN,
+    leg_space               DECIMAL,
+    has_power               BOOLEAN,
+    is_by_window            BOOLEAN,
+    is_by_aisle             BOOLEAN,
+    is_between_seats        BOOLEAN,
+    is_in_front_row         BOOLEAN,
+    is_in_end_row           BOOLEAN,
+    is_facing_window        BOOLEAN,
+    is_facing_aisle         BOOLEAN,
+    CONSTRAINT pk_locatablespot PRIMARY KEY (id)
 );
 
-
-CREATE TABLE spot_column
-(
-    id                  BIGINT NOT NULL,
-    created             TIMESTAMP WITHOUT TIME ZONE,
-    changed             TIMESTAMP WITHOUT TIME ZONE,
-    version             BIGINT NOT NULL,
-    netex_id            VARCHAR(255),
-    numbering_from_left BOOLEAN,
-    from_date           TIMESTAMP WITHOUT TIME ZONE,
-    to_date             TIMESTAMP WITHOUT TIME ZONE,
-    label_value         VARCHAR(255),
-    label_lang          VARCHAR(5),
-    CONSTRAINT pk_spotcolumn PRIMARY KEY (id)
-);
-
-ALTER TABLE spot_column OWNER TO sobek;
-
---
--- TOC entry 325 (class 1259 OID 20093)
--- Name: spot_row_seq; Type: SEQUENCE; Schema: public; Owner: sobek
---
-
-CREATE SEQUENCE public.spot_column_seq
+CREATE SEQUENCE locatable_spot_seq
     START WITH 1
     INCREMENT BY 10
     NO MINVALUE
@@ -198,57 +197,21 @@ CREATE SEQUENCE public.spot_column_seq
     CACHE 1;
 
 
-ALTER SEQUENCE public.spot_column_seq OWNER TO sobek;
+ALTER SEQUENCE locatable_spot_seq OWNER TO sobek;
 
-CREATE TABLE deck_spot_columns
+CREATE TABLE locatable_spot_actual_vehicle_equipments
 (
-    deck_id         BIGINT NOT NULL,
-    spot_columns_id BIGINT NOT NULL
-);
-
-
-ALTER TABLE passenger_spot
-    ADD spot_column_id BIGINT;
-
-ALTER TABLE passenger_spot
-    ADD spot_row_id BIGINT;
-
-ALTER TABLE deck_spot_columns
-    ADD CONSTRAINT uc_deck_spot_columns_spotcolumns UNIQUE (spot_columns_id);
-
-
-CREATE SEQUENCE IF NOT EXISTS public.luggage_spot_seq
-    INCREMENT 10
-    START 1
-    MINVALUE 1
-    MAXVALUE 9223372036854775807
-    CACHE 1;
-
-ALTER SEQUENCE public.luggage_spot_seq
-    OWNER TO sobek;
-
-CREATE TABLE luggage_spot_actual_vehicle_equipments
-(
-    luggage_spot_id              BIGINT NOT NULL,
+    locatable_spot_id            BIGINT NOT NULL,
     actual_vehicle_equipments_id BIGINT NOT NULL
 );
 
-CREATE TABLE luggage_spot_key_values
+CREATE TABLE locatable_spot_key_values
 (
-    luggage_spot_id BIGINT       NOT NULL,
-    key_values_id   BIGINT       NOT NULL,
-    key_values_key  VARCHAR(255) NOT NULL,
-    CONSTRAINT pk_luggage_spot_keyvalues PRIMARY KEY (luggage_spot_id, key_values_key)
+    locatable_spot_id BIGINT       NOT NULL,
+    key_values_id     BIGINT       NOT NULL,
+    key_values_key    VARCHAR(255) NOT NULL,
+    CONSTRAINT pk_locatable_spot_keyvalues PRIMARY KEY (locatable_spot_id, key_values_key)
 );
-
-CREATE TABLE public.passenger_space_luggage_spots (
-                                                        passenger_space_id bigint NOT NULL,
-                                                        luggage_spots_id   bigint NOT NULL
-);
-
-
-ALTER TABLE public.passenger_space_luggage_spots OWNER TO sobek;
-
 
 CREATE TABLE passenger_entrance_actual_vehicle_equipments
 (
@@ -262,80 +225,11 @@ CREATE TABLE passenger_space_actual_vehicle_equipments
     actual_vehicle_equipments_id BIGINT NOT NULL
 );
 
-CREATE TABLE passenger_spot_actual_vehicle_equipments
+CREATE TABLE passenger_space_luggage_spots
 (
-    passenger_spot_id            BIGINT NOT NULL,
-    actual_vehicle_equipments_id BIGINT NOT NULL
+    passenger_space_id BIGINT NOT NULL,
+    luggage_spots_id   BIGINT NOT NULL
 );
-
-ALTER TABLE luggage_spot_key_values
-    ADD CONSTRAINT uc_luggage_spot_key_values_keyvalues UNIQUE (key_values_id);
-
-ALTER TABLE luggage_spot_actual_vehicle_equipments
-    ADD CONSTRAINT uc_luggagespotactualvehicleequipments_actualvehicleequipments UNIQUE (actual_vehicle_equipments_id, luggage_spot_id);
-
-ALTER TABLE passenger_entrance_actual_vehicle_equipments
-    ADD CONSTRAINT uc_passengerentranceactualvehicleequipm_actualvehicleequipments UNIQUE (actual_vehicle_equipments_id, passenger_entrance_id);
-
-ALTER TABLE passenger_space_actual_vehicle_equipments
-    ADD CONSTRAINT uc_passengerspaceactualvehicleequipment_actualvehicleequipments UNIQUE (actual_vehicle_equipments_id, passenger_space_id);
-
-ALTER TABLE passenger_spot_actual_vehicle_equipments
-    ADD CONSTRAINT uc_passengerspotactualvehicleequipments_actualvehicleequipments UNIQUE (actual_vehicle_equipments_id, passenger_spot_id);
-
-ALTER TABLE luggage_spot
-    ADD CONSTRAINT FK_LUGGAGESPOT_ON_POLYGON FOREIGN KEY (polygon_id) REFERENCES persistable_polygon (id);
-
-ALTER TABLE luggage_spot_actual_vehicle_equipments
-    ADD CONSTRAINT fk_lugspoactvehequ_on_equipment FOREIGN KEY (actual_vehicle_equipments_id) REFERENCES equipment (id);
-
-ALTER TABLE luggage_spot_actual_vehicle_equipments
-    ADD CONSTRAINT fk_lugspoactvehequ_on_luggage_spot FOREIGN KEY (luggage_spot_id) REFERENCES luggage_spot (id);
-
-ALTER TABLE luggage_spot_key_values
-    ADD CONSTRAINT fk_lugspokeyval_on_luggage_spot FOREIGN KEY (luggage_spot_id) REFERENCES luggage_spot (id);
-
-ALTER TABLE luggage_spot_key_values
-    ADD CONSTRAINT fk_lugspokeyval_on_value FOREIGN KEY (key_values_id) REFERENCES value (id);
-
-ALTER TABLE passenger_entrance_actual_vehicle_equipments
-    ADD CONSTRAINT fk_pasentactvehequ_on_equipment FOREIGN KEY (actual_vehicle_equipments_id) REFERENCES equipment (id);
-
-ALTER TABLE passenger_entrance_actual_vehicle_equipments
-    ADD CONSTRAINT fk_pasentactvehequ_on_passenger_entrance FOREIGN KEY (passenger_entrance_id) REFERENCES passenger_entrance (id);
-
-ALTER TABLE passenger_space_actual_vehicle_equipments
-    ADD CONSTRAINT fk_passpaactvehequ_on_equipment FOREIGN KEY (actual_vehicle_equipments_id) REFERENCES equipment (id);
-
-ALTER TABLE passenger_space_actual_vehicle_equipments
-    ADD CONSTRAINT fk_passpaactvehequ_on_passenger_space FOREIGN KEY (passenger_space_id) REFERENCES passenger_space (id);
-
-ALTER TABLE passenger_spot_actual_vehicle_equipments
-    ADD CONSTRAINT fk_passpoactvehequ_on_equipment FOREIGN KEY (actual_vehicle_equipments_id) REFERENCES equipment (id);
-
-ALTER TABLE passenger_spot_actual_vehicle_equipments
-    ADD CONSTRAINT fk_passpoactvehequ_on_passenger_spot FOREIGN KEY (passenger_spot_id) REFERENCES passenger_spot (id);
-
-ALTER TABLE luggage_spot
-    ADD CONSTRAINT FK_LUGGAGESPOT_ON_SPOTCOLUMN FOREIGN KEY (spot_column_id) REFERENCES spot_column (id);
-
-ALTER TABLE luggage_spot
-    ADD CONSTRAINT FK_LUGGAGESPOT_ON_SPOTROW FOREIGN KEY (spot_row_id) REFERENCES spot_row (id);
-
-ALTER TABLE passenger_spot
-    ADD CONSTRAINT FK_PASSENGERSPOT_ON_SPOTCOLUMN FOREIGN KEY (spot_column_id) REFERENCES spot_column (id);
-
-ALTER TABLE passenger_spot
-    ADD CONSTRAINT FK_PASSENGERSPOT_ON_SPOTROW FOREIGN KEY (spot_row_id) REFERENCES spot_row (id);
-
-ALTER TABLE deck_spot_columns
-    ADD CONSTRAINT fk_decspocol_on_deck FOREIGN KEY (deck_id) REFERENCES deck (id);
-
-ALTER TABLE deck_spot_columns
-    ADD CONSTRAINT fk_decspocol_on_spot_column FOREIGN KEY (spot_columns_id) REFERENCES spot_column (id);
-
-ALTER TABLE passenger_spot
-    DROP COLUMN spot_row_ref;
 
 CREATE TABLE passenger_space_spot_affinities
 (
@@ -363,6 +257,16 @@ CREATE TABLE spot_affinity
     CONSTRAINT pk_spotaffinity PRIMARY KEY (id)
 );
 
+CREATE SEQUENCE spot_affinity_seq
+    START WITH 1
+    INCREMENT BY 10
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE spot_affinity_seq OWNER TO sobek;
+
 CREATE TABLE spot_affinity_key_values
 (
     spot_affinity_id BIGINT       NOT NULL,
@@ -371,11 +275,117 @@ CREATE TABLE spot_affinity_key_values
     CONSTRAINT pk_spot_affinity_keyvalues PRIMARY KEY (spot_affinity_id, key_values_key)
 );
 
+CREATE TABLE spot_affinity_member
+(
+    locatable_spot_id BIGINT NOT NULL,
+    spot_affinity_id  BIGINT NOT NULL
+);
+
+CREATE TABLE spot_column
+(
+    id                  BIGINT NOT NULL,
+    created             TIMESTAMP WITHOUT TIME ZONE,
+    changed             TIMESTAMP WITHOUT TIME ZONE,
+    version             BIGINT NOT NULL,
+    netex_id            VARCHAR(255),
+    numbering_from_left BOOLEAN,
+    from_date           TIMESTAMP WITHOUT TIME ZONE,
+    to_date             TIMESTAMP WITHOUT TIME ZONE,
+    label_value         VARCHAR(255),
+    label_lang          VARCHAR(5),
+    CONSTRAINT pk_spotcolumn PRIMARY KEY (id)
+);
+
+CREATE SEQUENCE spot_column_seq
+    START WITH 1
+    INCREMENT BY 10
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE spot_column_seq OWNER TO sobek;
+
+ALTER TABLE deck_spot_columns
+    ADD CONSTRAINT uc_deck_spot_columns_spotcolumns UNIQUE (spot_columns_id);
+
+ALTER TABLE equipment_key_values
+    ADD CONSTRAINT uc_equipment_key_values_keyvalues UNIQUE (key_values_id);
+
+ALTER TABLE locatable_spot_key_values
+    ADD CONSTRAINT uc_locatable_spot_key_values_keyvalues UNIQUE (key_values_id);
+
+ALTER TABLE locatable_spot_actual_vehicle_equipments
+    ADD CONSTRAINT uc_locatablespotactualvehicleequipments_actualvehicleequipments UNIQUE (actual_vehicle_equipments_id, locatable_spot_id);
+
+ALTER TABLE passenger_space_luggage_spots
+    ADD CONSTRAINT uc_passenger_space_luggage_spots_luggagespots UNIQUE (luggage_spots_id);
+
 ALTER TABLE passenger_space_spot_affinities
     ADD CONSTRAINT uc_passenger_space_spot_affinities_spotaffinities UNIQUE (spot_affinities_id);
 
+ALTER TABLE passenger_entrance_actual_vehicle_equipments
+    ADD CONSTRAINT uc_passengerentranceactualvehicleequipm_actualvehicleequipments UNIQUE (actual_vehicle_equipments_id, passenger_entrance_id);
+
+ALTER TABLE passenger_space_actual_vehicle_equipments
+    ADD CONSTRAINT uc_passengerspaceactualvehicleequipment_actualvehicleequipments UNIQUE (actual_vehicle_equipments_id, passenger_space_id);
+
 ALTER TABLE spot_affinity_key_values
     ADD CONSTRAINT uc_spot_affinity_key_values_keyvalues UNIQUE (key_values_id);
+
+ALTER TABLE locatable_spot
+    ADD CONSTRAINT FK_LOCATABLESPOT_ON_POLYGON FOREIGN KEY (polygon_id) REFERENCES persistable_polygon (id);
+
+ALTER TABLE locatable_spot
+    ADD CONSTRAINT FK_LOCATABLESPOT_ON_SPOTCOLUMN FOREIGN KEY (spot_column_id) REFERENCES spot_column (id);
+
+ALTER TABLE locatable_spot
+    ADD CONSTRAINT FK_LOCATABLESPOT_ON_SPOTROW FOREIGN KEY (spot_row_id) REFERENCES spot_row (id);
+
+ALTER TABLE deck_spot_columns
+    ADD CONSTRAINT fk_decspocol_on_deck FOREIGN KEY (deck_id) REFERENCES deck (id);
+
+ALTER TABLE deck_spot_columns
+    ADD CONSTRAINT fk_decspocol_on_spot_column FOREIGN KEY (spot_columns_id) REFERENCES spot_column (id);
+
+ALTER TABLE equipment_key_values
+    ADD CONSTRAINT fk_equkeyval_on_equipment FOREIGN KEY (equipment_id) REFERENCES equipment (id);
+
+ALTER TABLE equipment_key_values
+    ADD CONSTRAINT fk_equkeyval_on_value FOREIGN KEY (key_values_id) REFERENCES value (id);
+
+ALTER TABLE locatable_spot_actual_vehicle_equipments
+    ADD CONSTRAINT fk_locspoactvehequ_on_equipment FOREIGN KEY (actual_vehicle_equipments_id) REFERENCES equipment (id);
+
+ALTER TABLE locatable_spot_actual_vehicle_equipments
+    ADD CONSTRAINT fk_locspoactvehequ_on_locatable_spot FOREIGN KEY (locatable_spot_id) REFERENCES locatable_spot (id);
+
+ALTER TABLE locatable_spot_key_values
+    ADD CONSTRAINT fk_locspokeyval_on_locatable_spot FOREIGN KEY (locatable_spot_id) REFERENCES locatable_spot (id);
+
+ALTER TABLE locatable_spot_key_values
+    ADD CONSTRAINT fk_locspokeyval_on_value FOREIGN KEY (key_values_id) REFERENCES value (id);
+
+ALTER TABLE passenger_entrance_actual_vehicle_equipments
+    ADD CONSTRAINT fk_pasentactvehequ_on_equipment FOREIGN KEY (actual_vehicle_equipments_id) REFERENCES equipment (id);
+
+ALTER TABLE passenger_entrance_actual_vehicle_equipments
+    ADD CONSTRAINT fk_pasentactvehequ_on_passenger_entrance FOREIGN KEY (passenger_entrance_id) REFERENCES passenger_entrance (id);
+
+ALTER TABLE passenger_space_actual_vehicle_equipments
+    ADD CONSTRAINT fk_passpaactvehequ_on_equipment FOREIGN KEY (actual_vehicle_equipments_id) REFERENCES equipment (id);
+
+ALTER TABLE passenger_space_actual_vehicle_equipments
+    ADD CONSTRAINT fk_passpaactvehequ_on_passenger_space FOREIGN KEY (passenger_space_id) REFERENCES passenger_space (id);
+
+ALTER TABLE passenger_space_luggage_spots
+    ADD CONSTRAINT fk_passpalugspo_on_luggage_spot FOREIGN KEY (luggage_spots_id) REFERENCES locatable_spot (id);
+
+ALTER TABLE passenger_space_luggage_spots
+    ADD CONSTRAINT fk_passpalugspo_on_passenger_space FOREIGN KEY (passenger_space_id) REFERENCES passenger_space (id);
+
+ALTER TABLE passenger_space_passenger_spots
+    ADD CONSTRAINT fk_passpapasspo_on_passenger_spot FOREIGN KEY (passenger_spots_id) REFERENCES locatable_spot (id);
 
 ALTER TABLE passenger_space_spot_affinities
     ADD CONSTRAINT fk_passpaspoaff_on_passenger_space FOREIGN KEY (passenger_space_id) REFERENCES passenger_space (id);
@@ -389,13 +399,14 @@ ALTER TABLE spot_affinity_key_values
 ALTER TABLE spot_affinity_key_values
     ADD CONSTRAINT fk_spoaffkeyval_on_value FOREIGN KEY (key_values_id) REFERENCES value (id);
 
-CREATE SEQUENCE IF NOT EXISTS public.spot_affinity_seq
-    INCREMENT 10
-    START 1
-    MINVALUE 1
-    MAXVALUE 9223372036854775807
-    CACHE 1;
+ALTER TABLE spot_affinity_member
+    ADD CONSTRAINT fk_spoaffmem_on_locatable_spot FOREIGN KEY (locatable_spot_id) REFERENCES locatable_spot (id);
 
-ALTER SEQUENCE public.spot_affinity_seq
-    OWNER TO sobek;
+ALTER TABLE spot_affinity_member
+    ADD CONSTRAINT fk_spoaffmem_on_spot_affinity FOREIGN KEY (spot_affinity_id) REFERENCES spot_affinity (id);
 
+DROP TABLE installed_equipment_version_structure CASCADE;
+
+DROP TABLE passenger_spot CASCADE;
+
+DROP TABLE passenger_spot_key_values CASCADE;

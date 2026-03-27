2e7f1b852b56f10409945730c46529e69cd9577f
Gunnar Gudlaugsson <gunnar.gudlaugsson@egde.no>
2026-03-27 09:36:54 +0100

    Spring Boot 4 stuff

--- sobek-service/src/main/java/org/rutebanken/sobek/changelog/LocalChangelogConfiguration.java
+++ sobek-service/src/main/java/org/rutebanken/sobek/changelog/LocalChangelogConfiguration.java
@@ -1,8 +1,6 @@
 package org.rutebanken.sobek.changelog;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jms.JmsAutoConfiguration;
-import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -10,7 +8,7 @@ import org.springframework.context.annotation.Profile;
  * This configuration disables conflicting autoconfiguration used by other variants of the changelog feature.
  */
 @Configuration
-@EnableAutoConfiguration(exclude = {JmsAutoConfiguration.class, ActiveMQAutoConfiguration.class})
+@EnableAutoConfiguration
 @Profile("local-changelog")
 public class LocalChangelogConfiguration {
     // intentionally left blank

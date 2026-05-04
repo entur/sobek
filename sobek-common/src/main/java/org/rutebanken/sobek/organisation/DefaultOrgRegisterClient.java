b33c8807e53e8b7e2ae75ef92351c62d932297d6
Gunnar Gudlaugsson <gunnar.gudlaugsson@egde.no>
2026-05-04 15:07:55 +0200

    Add webclient, with specific settings fro max memory size to prevent error, because the dataset in Norway is bigger than the default 261K limit

---
+++ sobek-common/src/main/java/org/rutebanken/sobek/organisation/DefaultOrgRegisterClient.java
@@ -0,0 +1,29 @@
+package org.rutebanken.sobek.organisation;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class DefaultOrgRegisterClient {
+
+    @Value("${netex.organisations.max-in-memory-size-kb:500}")
+    String maxInMemorySizeKB;
+
+    @Bean("orgRegisterClient")
+    @ConditionalOnMissingBean(name = "orgRegisterClient")
+    WebClient orgRegisterClient(WebClient.Builder webClientBuilder) {
+        return webClientBuilder
+                .defaultHeader("Et-Client-Name", "entur-sobek")
+                .exchangeStrategies(ExchangeStrategies
+                        .builder()
+                        .codecs(codecs -> codecs
+                                .defaultCodecs()
+                                .maxInMemorySize(Long.valueOf(maxInMemorySizeKB).intValue() * 1024))
+                        .build())
+                .build();
+    }
+}

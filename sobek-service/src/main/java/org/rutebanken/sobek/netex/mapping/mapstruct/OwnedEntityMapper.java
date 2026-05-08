c1cad4a507d12119014478a9ae86831b97e8a087
Gunnar Gudlaugsson <gunnar.gudlaugsson@egde.no>
2026-05-08 12:06:44 +0200

    Remove Mapstruct annotations

--- sobek-service/src/main/java/org/rutebanken/sobek/netex/mapping/mapstruct/OwnedEntityMapper.java
+++ sobek-service/src/main/java/org/rutebanken/sobek/netex/mapping/mapstruct/OwnedEntityMapper.java
@@ -1,7 +1,5 @@
 package org.rutebanken.sobek.netex.mapping.mapstruct;
 
-import org.mapstruct.Context;
-import org.mapstruct.MappingTarget;
 import org.rutebanken.sobek.model.authorization.OwnedEntity;
 import org.rutebanken.sobek.netex.mapping.context.MappingContext;
 import org.springframework.stereotype.Service;
@@ -10,8 +8,8 @@ import org.springframework.stereotype.Service;
 public class OwnedEntityMapper {
 
     public void updateSobekFromNetex(
-            @MappingTarget OwnedEntity target,
-            @Context MappingContext context
+            OwnedEntity target,
+            MappingContext context
     ) {
         if (target != null) {
             target.setDataOwnerRef(context.getDataOwnerRef());

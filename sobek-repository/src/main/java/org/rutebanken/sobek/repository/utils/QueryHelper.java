93bee844a9746ad62ffbc0cc8db914071d7535c1
Gunnar Gudlaugsson <gunnar.gudlaugsson@egde.no>
2026-05-27 14:25:04 +0200

    Fix https://github.com/entur/sobek/issues/129 - return vehicles that have no vehicle type

--- sobek-repository/src/main/java/org/rutebanken/sobek/repository/utils/QueryHelper.java
+++ sobek-repository/src/main/java/org/rutebanken/sobek/repository/utils/QueryHelper.java
@@ -2,7 +2,7 @@ package org.rutebanken.sobek.repository.utils;
 
 public class QueryHelper {
     public static String objectValidCondition(String tableAlias, String parameterName) {
-        return String.format("%s.validBetween.fromDate <= :%s " +
-            "AND (%s.validBetween.toDate IS NULL OR %s.validBetween.toDate >= :%s)", tableAlias, parameterName, tableAlias, tableAlias, parameterName);
+        return String.format("(%s.validBetween.fromDate IS NULL or %s.validBetween.fromDate  <= :%s) " +
+            "AND (%s.validBetween.toDate IS NULL OR %s.validBetween.toDate >= :%s)", tableAlias, tableAlias, parameterName, tableAlias, tableAlias, parameterName);
     }
 }

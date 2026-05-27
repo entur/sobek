261feca0e73eb7c8e7c84a9c2f01ca7cb5969523
Gunnar Gudlaugsson <gunnar.gudlaugsson@egde.no>
2026-05-27 12:55:35 +0200

    Add filter for netexIds in DeckPlan GraphQL

--- sobek-repository/src/main/java/org/rutebanken/sobek/repository/DeckPlanRepositoryImpl.java
+++ sobek-repository/src/main/java/org/rutebanken/sobek/repository/DeckPlanRepositoryImpl.java
@@ -63,28 +63,45 @@ public class DeckPlanRepositoryImpl implements DeckPlanRepositoryCustom {
 
     @Override
     public List<DeckPlan> findAllCurrent() {
-        return findCurrentPaged(Pageable.unpaged()).getContent();
+        return findCurrentPaged(null, Pageable.unpaged()).getContent();
     }
 
     @Override
-    public Page<DeckPlan> findCurrentPaged(Pageable pageable) {
+    public Page<DeckPlan> findCurrentPaged(List<String> netexIds, Pageable pageable) {
         Instant now = Instant.now();
-        String baseJpql = "SELECT DISTINCT dp FROM DeckPlan dp " + CURRENT_BASE_WHERE;
+
+        StringBuilder filterSuffix = new StringBuilder();
+        if (netexIds != null && !netexIds.isEmpty()) {
+            filterSuffix.append(" AND dp.netexId IN :netexIds");
+        }
+
+
+        String baseJpql = "SELECT DISTINCT dp FROM DeckPlan dp " + CURRENT_BASE_WHERE + filterSuffix;
 
         if (pageable.isUnpaged()) {
             TypedQuery<DeckPlan> query = entityManager.createQuery(baseJpql, DeckPlan.class);
             query.setParameter("now", now);
+            if (netexIds != null && !netexIds.isEmpty()) {
+                query.setParameter("netexIds", netexIds);
+            }
             List<DeckPlan> results = query.getResultList();
             return new PageImpl<>(results);
         }
 
-        String countJpql = "SELECT COUNT(DISTINCT dp) FROM DeckPlan dp " + CURRENT_BASE_WHERE;
+        String countJpql = "SELECT COUNT(DISTINCT dp) FROM DeckPlan dp " + CURRENT_BASE_WHERE + filterSuffix;
         TypedQuery<Long> countQuery = entityManager.createQuery(countJpql, Long.class);
         countQuery.setParameter("now", now);
+        if (netexIds != null && !netexIds.isEmpty()) {
+            countQuery.setParameter("netexIds", netexIds);
+        }
+
         long total = countQuery.getSingleResult();
 
         TypedQuery<DeckPlan> query = entityManager.createQuery(baseJpql + " ORDER BY dp.id", DeckPlan.class);
         query.setParameter("now", now);
+        if (netexIds != null && !netexIds.isEmpty()) {
+            query.setParameter("netexIds", netexIds);
+        }
         query.setFirstResult((int) pageable.getOffset());
         query.setMaxResults(pageable.getPageSize());
 

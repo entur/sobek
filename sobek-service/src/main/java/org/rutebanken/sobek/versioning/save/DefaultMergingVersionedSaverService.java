2d0df207b371a053c094f483a84886dff1eb496b
Gunnar Gudlaugsson <gunnar.gudlaugsson@egde.no>
2026-07-22 17:31:41 +0200

    Remove unused import

--- sobek-service/src/main/java/org/rutebanken/sobek/versioning/save/DefaultMergingVersionedSaverService.java
+++ sobek-service/src/main/java/org/rutebanken/sobek/versioning/save/DefaultMergingVersionedSaverService.java
@@ -29,7 +29,6 @@ import org.rutebanken.sobek.versioning.VersionIncrementor;
 import org.rutebanken.sobek.versioning.validate.VersionValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;

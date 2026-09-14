1d898c9a48736aa5300e7ab77c6215c7b5968180
Gunnar Gudlaugsson <gunnar.gudlaugsson@egde.no>
2026-09-14 15:33:16 +0200

    Fix PolygonMapper so that the order of points there is x,y instead of y,x

--- sobek-service/src/main/java/org/rutebanken/sobek/netex/mapping/mapstruct/PolygonMapper.java
+++ sobek-service/src/main/java/org/rutebanken/sobek/netex/mapping/mapstruct/PolygonMapper.java
@@ -134,8 +134,8 @@ public abstract class PolygonMapper {
     protected List<Double> toList(Coordinate[] coordinates) {
         List<Double> values = new ArrayList<>(coordinates.length * 2);
         for (Coordinate coordinate : coordinates) {
-            values.add(coordinate.y);
             values.add(coordinate.x);
+            values.add(coordinate.y);
         }
         return values;
     }
